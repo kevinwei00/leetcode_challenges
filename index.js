3bb2f35ece18bacf83d6b2762c4cf6f476793467
Kevin Wei <chenghaowei@gmail.com>
2020-01-19 16:49:22 -0500

    reverse linked list

--- index.js
+++ index.js
@@ -425,3 +425,32 @@ function groupAnagrams(strs) {
 
   return arr;
 }
+
+/* Reverse a Linked List
+
+Reverse a singly linked list.
+
+Example:
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+
+Follow up:
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+*/
+
+function reverseList(head) {
+  let curr = head;
+  let prev = null;
+  let next = curr ? curr.next : null;
+  while (next !== null) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  if (prev) {
+    head = prev;
+  }
+  return head;
+}
