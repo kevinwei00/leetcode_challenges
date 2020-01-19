c94d59ec6ba7206e7263b599a44caf8b5b7feb46
Kevin Wei <chenghaowei@gmail.com>
2020-01-19 17:09:39 -0500

    detect cyclic linked list

--- index.js
+++ index.js
@@ -454,3 +454,49 @@ function reverseList(head) {
   }
   return head;
 }
+
+/* Linked List Cycle
+
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the
+position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is
+no cycle in the linked list.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+Example 2:
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+Example 3:
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+Follow up:
+Can you solve it using O(1) (i.e. constant) memory?
+
+*/
+
+function hasCycle(head) {
+  if (!head) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+}
+
