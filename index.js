3bf5680b52f22f7913a087603b1e1c8444862e70
Kevin Wei <chenghaowei@gmail.com>
2020-01-22 23:33:28 -0500

    refactor num islands

--- index.js
+++ index.js
@@ -743,34 +743,31 @@ Input:
 Output: 3
 
 */
-// recursive depth first search
+// DEPTH FIRST SEARCH
 function numIslands(grid) {
   function dfs(row, col) {
-    if (grid[row][col] === '1') {
-      grid[row][col] = '0';
-
-      // top
-      if (row > 0) {
-        dfs(row - 1, col);
-      }
-      // left
-      if (col > 0) {
-        dfs(row, col - 1);
-      }
-      // bottom
-      if (row < grid.length - 1) {
-        dfs(row + 1, col);
-      }
-      // right
-      if (col < grid[row].length - 1) {
-        dfs(row, col + 1);
-      }
+    // out of bounds
+    if (row < 0 || col < 0 || row > grid.length - 1 || col > grid[0].length - 1) {
+      return;
     }
+    // already visited
+    if (grid[row][col] === '0') {
+      return;
+    }
+
+    // set to visited
+    grid[row][col] = '0';
+
+    // recurse top, bottom, left, right
+    dfs(row - 1, col);
+    dfs(row + 1, col);
+    dfs(row, col - 1);
+    dfs(row, col + 1);
   }
 
   let total = 0;
   for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
+    for (let j = 0; j < grid[0].length; j++) {
       if (grid[i][j] === '1') {
         total++;
         dfs(i, j);
@@ -876,3 +873,38 @@ function countSubstrings(s) {
         x       x
                 lr
 */
+
+/* Pacific Atlantic Water Flow
+
+Given an m x n matrix of non-negative integers representing the height of each unit cell in a 
+continent, the "Pacific ocean" touches the left and top edges of the matrix and the "Atlantic
+ocean" touches the right and bottom edges.
+
+Water can only flow in four directions (up, down, left, or right) from a cell to another one
+with height equal or lower.
+
+Find the list of grid coordinates where water can flow to both the Pacific and Atlantic ocean.
+
+Note:
+The order of returned grid coordinates does not matter.
+Both m and n are less than 150.
+
+Example:
+
+Given the following 5x5 matrix:
+
+  Pacific ~   ~   ~   ~   ~ 
+       ~  1   2   2   3  (5) *
+       ~  3   2   3  (4) (4) *
+       ~  2   4  (5)  3   1  *
+       ~ (6) (7)  1   4   5  *
+       ~ (5)  1   1   2   4  *
+          *   *   *   *   * Atlantic
+
+Return:
+[[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]]
+(positions with parentheses in above matrix).
+
+*/
+// DEPTH FIRST SEARCH
+function pacificAtlantic(matrix) {}
