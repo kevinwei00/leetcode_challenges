2199a06b75a7ed9b149aa8b58b9c41bdf06c6970
Kevin Wei <chenghaowei@gmail.com>
2020-01-22 19:46:13 -0500

    palindromic substrings

--- index.js
+++ index.js
@@ -820,3 +820,42 @@ function removeNthFromEnd(head, n) {
   slow.next = slow.next.next;
   return head;
 }
+
+/* Palindromic Substrings
+
+Given a string, your task is to count how many palindromic substrings in this string.
+
+The substrings with different start indexes or end indexes are counted as different
+substrings even they consist of same characters.
+
+Example 1:
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+
+Example 2:
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+Note:
+The input string length won't exceed 1000.
+
+*/
+
+function countSubstrings(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    helper(i, i);
+    helper(i, i + 1);
+  }
+  return count;
+
+  function helper(left, right) {
+    while (left >= 0 && right <= s.length && s[left] === s[right]) {
+      count++;
+      left--;
+      right++;
+    }
+  }
+}
